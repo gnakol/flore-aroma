40fbc548f94d4971647efaffd6c06067b3440797
gnakol <gna.kolie@yahoo.fr>
2025-10-26 19:30:26 +0100

    feat: add docker-compose and initial API structure

---
+++ api/flore-aroma/src/main/java/fr/ecommerce/flore/aroma/flore_aroma/FloreAromaApplication.java
@@ -0,0 +1,13 @@
+package fr.ecommerce.flore.aroma.flore_aroma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloreAromaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloreAromaApplication.class, args);
+	}
+
+}
---
+++ api/flore-aroma/src/test/java/fr/ecommerce/flore/aroma/flore_aroma/FloreAromaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ecommerce.flore.aroma.flore_aroma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloreAromaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
